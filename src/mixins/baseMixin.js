eef3d19ba68a5ce1996dc38d870981f00192e8c2
qijianyi <245690826@qq.com>
2019-07-03 00:39:45 +0800

    111111111111111111111111111111111

--- src/mixins/baseMixin.js
+++ src/mixins/baseMixin.js
@@ -1,9 +1,6 @@
 import wepy from 'wepy'
 import util from '@/utils/util'
 export default class baseMixin extends wepy.mixin {
-  $alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
-    return util.alert(content, title, confirmText, confirmColor);
-  }
   $ajax(url, params = {}) {
     return util.ajax(url, params);
   }
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,20 +1,5 @@
 import wepy from 'wepy'
 export default class Util {
-  static alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
-    return new Promise((resolve, reject) => {
-      wx.showModal({
-        title: title,
-        content: content,
-        confirmText: confirmText,
-        confirmColor: confirmColor,
-        mask: true,
-        showCancel: false,
-        success: res => {
-          resolve();
-        }
-      });
-    });
-  }
   static async ajax(url, params = {}) {
     params = {
       ...{
@@ -24,11 +9,24 @@ export default class Util {
         fail: () => {},
         error: () => {},
         complete: () => {},
-      },
-      ...params
-    };
-    let res = await wepy.request(params);
+      },...params
+    }
+    return await wepy.request(params);
+    // console.log(res);
+    // return res;
+    // params = {
+    //   ...{
+    //     url: url,
+    //     method: 'GET',
+    //     success: () => {},
+    //     fail: () => {},
+    //     error: () => {},
+    //     complete: () => {},
+    //   },
+    //   ...params
+    // };
+  
 
-    return res && (res.statusCode == 200 ? params.success(res.data) : params.fail(res.data));
+    // return res && (res.statusCode == 200 ? params.success(res.data) : params.fail(res.data));
   }
 }
