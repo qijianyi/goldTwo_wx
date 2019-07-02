4339c87909e293da9fa20f14f9f3b509c003feb8
qijianyi <245690826@qq.com>
2019-07-02 13:00:48 +0800

    1111111111111111111111111111111111111

--- src/mixins/baseMixin.js
+++ src/mixins/baseMixin.js
@@ -1,7 +1,10 @@
 import wepy from 'wepy'
 import util from '@/utils/util'
 export default class baseMixin extends wepy.mixin {
-  async $ajax(url, params = {}) {
+  $alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
+    return util.alert(content, title, confirmText, confirmColor);
+  }
+  $ajax(url, params = {}) {
     return util.ajax(url, params);
   }
 }
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,76 +1,34 @@
 import wepy from 'wepy'
 export default class Util {
-
-  static isArray(item) {
-    return Object.prototype.toString.apply(item) === '[object Array]';
+  static alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
+    return new Promise((resolve, reject) => {
+      wx.showModal({
+        title: title,
+        content: content,
+        confirmText: confirmText,
+        confirmColor: confirmColor,
+        mask: true,
+        showCancel: false,
+        success: res => {
+          resolve();
+        }
+      });
+    });
   }
-  static isObject(item) {
-    return typeof item === 'object' && !this.isArray(item);
-  }
-  static isEmpty(item) {
-    return !(this.isStr(item) && item.length > 0);
-  }
-  static isStr(item) {
-    return typeof item === 'string';
-  }
-  static isFunc(item) {
-    return typeof item === 'function';
-  }
-  static notEmpty(str) {
-    return this.isStr(str) && str.length > 0;
-  }
-  static isEmpty(str) {
-    return !this.notEmpty(str);
-  }
-
-
-
-  static async http(url, params = {}) {
-    let data = params.query || {},
-      p = {
-        url: url,
-        method: params.method,
-        data: data,
-        header: {
-          ...{},
-          ...params.header
-        },
-        count: params.count || 1,
-        file: params.file,
-        progress: params.progress,
-      },
-      res = await this._request(p);
-
+  static async ajax(url, params = {}) {
     params = {
       ...{
+        url: url,
+        method: 'GET',
         success: () => {},
-        fail: (res) => {
-
-        },
-        error: (res) => {
-
-        },
+        fail: () => {},
+        error: () => {},
         complete: () => {},
       },
       ...params
     };
-    return res && params.success(res.data);
-  }
+    let res = await wepy.request(params);
 
-
-  static async _request(params) {
-    return await wepy.request(params);
-  }
-
-  static async ajax(url, params = {}) {
-    return this.http(url, {
-      ...params,
-      ...{
-        method: 'GET',
-        header: {
-          'Content-Type': params.file ? 'multipart/form-data;application/json' : 'application/x-www-form-urlencoded'
-        }
-      }
-    });
+    return res && (res.statusCode == 200 ? params.success(res.data) : params.fail(res.data));
   }
 }
