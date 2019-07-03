01cfbf1b43b8b1e2ebdeeb77739d4f0e87123316
qijianyi <245690826@qq.com>
2019-07-03 18:23:29 +0800

    ssssssssssssssssssssssssssssssssssssssssssssss

--- src/mixins/baseMixin.js
+++ src/mixins/baseMixin.js
@@ -1,6 +1,15 @@
 import wepy from 'wepy'
 import util from '@/utils/util'
 export default class baseMixin extends wepy.mixin {
+  $toast(title, icon = "success", duration = 2000) {
+    return util.toast(title, icon, duration);
+  }
+  $alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
+    return util.alert(content, title, confirmText, confirmColor);
+  }
+  $confirm(text, title = "提示", confirmText = '确定', confirmColor = '#000000') {
+    return util.confirm(text, title, confirmText, confirmColor);
+  }
   $ajax(url, params = {}) {
     return util.ajax(url, params);
   }
--- src/utils/util.js
+++ src/utils/util.js
@@ -1,32 +1,85 @@
 import wepy from 'wepy'
 export default class Util {
-  static async ajax(url, params = {}) {
-    params = {
-      ...{
+  static async ajax(url, params) {
+    wepy.showLoading({
+      title: '加载中...',
+      mask: true,
+      success: res => {}
+    });
+    let network = await new Promise((resolve, reject) => {
+      wepy.request({
         url: url,
+        data: params.data,
         method: 'GET',
-        success: () => {},
-        fail: () => {},
-        error: () => {},
-        complete: () => {},
-      },...params
-    }
-    return await wepy.request(params);
-    // console.log(res);
-    // return res;
-    // params = {
-    //   ...{
-    //     url: url,
-    //     method: 'GET',
-    //     success: () => {},
-    //     fail: () => {},
-    //     error: () => {},
-    //     complete: () => {},
-    //   },
-    //   ...params
-    // };
-  
-
-    // return res && (res.statusCode == 200 ? params.success(res.data) : params.fail(res.data));
+        dataType: 'json',
+        header: {
+          'content-type': 'applicction/x-www-form-urlencoded'
+        },
+        success: res => {
+          res.statusCode == 200 ? resolve(res.data) : reiect(res);
+        },
+        fail: res => {
+          this.toast('请求错误', 'none')
+          reiect(res);
+        },
+        complete: res => {
+          wepy.hideLoading();
+        }
+      });
+    })
+    return network;
+  }
+  static toast(title, icon = "success", duration = 2000) {
+    return new Promise((resolve, reject) => {
+      wepy.showToast({
+        title: title,
+        icon: icon,
+        duration: duration,
+        mask: true,
+        success: res => {
+          resolve();
+        }
+      })
+      setTimeout(() => {
+        wepy.hideToast();
+      }, duration)
+    });
+  }
+  static alert(content, title = '提示', confirmText = '确定', confirmColor = '#000000') {
+    return new Promise((resolve, reject) => {
+      wx.showModal({
+        title: title,
+        content: content,
+        confirmText: confirmText,
+        confirmColor: confirmColor,
+        mask: true,
+        showCancel: false,
+        success: res => {
+          resolve();
+        }
+      });
+    });
+  }
+  static confirm(text, title = "提示", confirmText = '确定', confirmColor = '#000000') {
+    return new Promise((resolve, reject) => {
+      wx.showModal({
+        title: title,
+        content: text,
+        confirmText: confirmText,
+        confirmColor: confirmColor,
+        mask: true,
+        showCancel: true,
+        success: res => {
+          if (res.confirm) {
+            resolve();
+          } else if (res.cancel) {
+            reject();
+          }
+        },
+        fail: res => {
+          reject();
+        }
+      });
+    });
   }
 }
